6dde6a226f7795b3f59a832ed7f2664432653bcf
Alexandre Boin <contact@alexandre-boin.fr>
2021-09-25 23:10:58 +0200

    :tada: Add first types

---
+++ index.ts
@@ -0,0 +1,53 @@
+export interface Permission {
+    id: string;
+    name: string;
+}
+
+export interface RoleType {
+    id: string;
+    name: string;
+    description: string;
+}
+
+export interface Role {
+    id: string;
+    type: RoleType;
+    displayName: string;
+    shortName?: string;
+    specificTo?: string;
+    suffix?: string;
+
+    color?: string;
+    icon?: string;
+    displayOnLogin: boolean;
+
+    hoist: boolean;
+    discordId?: string;
+    position?: number;
+
+    permissions?: Permission[]
+}
+
+export interface Institution {
+    id: string;
+    name: string;
+    authorizedDomains: string[];
+    casServer?: string;
+}
+
+export interface User {
+    username: string;
+    email: string;
+    photoURL?: string;
+    personalEmail?: string;
+    displayName: string;
+    firstName: string;
+    lastName: string;
+    customDisplayName?: string;
+    gender?: string;
+    groups?: Role[] | string[],
+    options?: Role[] | string[],
+    roles?: Role[] | string[],
+    permissions?: Role[] | string[],
+    discordId?: string;
+}
